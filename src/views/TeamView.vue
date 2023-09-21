f77c2e899ad3eb01a7264fc026f32454d60b05ed
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-09-21 14:47:24 +0300

    First Commit

---
+++ src/views/TeamView.vue
@@ -0,0 +1,41 @@
+<script setup>
+import TeamMember from '@/components/Teams/TeamMember.vue';
+</script>
+
+<template>
+  <header class="flex justify-between">
+    <div>
+        <button class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
+                :disabled="true">Add Member (0 Spots Left)</button>
+    </div>
+    
+    <div>
+        <h3 class="inline-flex items-center text-3xl relative">
+            <img src="/smiley.png" alt="" class="mr-2">
+            Smiley team
+        <div class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2">5</div>
+        </h3>
+    </div>
+  </header>
+
+  <div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
+    <table class="table-fixed border-spacing-2 border-separate">
+        <thead>
+            <th class="text-left px-6 py-2">Name</th>
+            <th class="text-left px-6 py-2">Email</th>
+            <th class="text-left px-6 py-2">Status</th>
+        </thead>
+
+        <tbody>
+            <TeamMember name="John Doe" email="john@doe.com" status="Active" />
+        </tbody>
+    </table>
+    
+    <p class="text-right text-gray-600 italic">There are no remaining team spots. Upgrade to add more.</p>
+  </div>
+
+
+  <footer class="mt-12 bg-gray-100 py-4 text-center">
+    <h5 class="font-semibold text-lg">Smiley - 5 Member Team</h5>
+  </footer>
+</template>
