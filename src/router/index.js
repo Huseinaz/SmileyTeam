c33894a5d16d5be78c7377e9c6c9e0fb06efe5ff
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-09-22 11:29:34 +0300

    Second Commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import ContactView from '@/views/ContactView.vue'
 import TeamView from '@/views/TeamView.vue'
 
 const router = createRouter({
@@ -9,24 +7,6 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: ContactView
-    },
-    {
-      path: '/team',
-      name: 'team',
       component: TeamView
     },
   ]
