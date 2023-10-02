258181f3f0ae8505847ba96dda5e5dd9d5bc8d38
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-10-02 13:07:54 +0300

    Transitions/Teleports

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -2,8 +2,7 @@
 import TeamFooter from '@/components/Teams/TeamFooter.vue';
 import TeamHeader from '@/components/Teams/TeamHeader.vue';
 import TeamMembers from '@/components/Teams/TeamMembers.vue';
-//import team from '@/team.json';
-import {useTeamStore} from "@/stores/TeamStore"
+import {useTeamStore} from "@/stores/TeamStore";
 
 let team = useTeamStore();
 team.fill();
@@ -17,4 +16,5 @@ team.fill();
   </div>
   
   <TeamFooter />
+
 </template>
